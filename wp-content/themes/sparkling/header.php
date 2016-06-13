24161da8fc42553e0155ebedaf9ec7bef7878ba6
root <root@omega-php1.(none)>
2016-06-13 19:48:25 +0200

    cleaning

--- wp-content/themes/sparkling/header.php
+++ wp-content/themes/sparkling/header.php
@@ -24,7 +24,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-
+<link href='https://fonts.googleapis.com/css?family=Lato:400,700,300,900,100&subset=latin-ext,latin' rel='stylesheet' type='text/css'>
 <?php wp_head(); ?>
 
 </head>
